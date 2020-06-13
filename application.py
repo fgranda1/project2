b0ab36abfad01f61f7df660c78190d145feb9a76
Felipe Granda R <felipegranda@hotmail.com>
2020-06-12 23:32:50 -0500

    added validation to adding channels, GUI fixes

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, url_for, render_template, request, redirect, flash
+from flask import Flask, url_for, render_template, request, redirect, flash, jsonify
 
 from flask_socketio import SocketIO, emit
 
@@ -12,15 +12,21 @@ socketio = SocketIO(app)
 
 
 # Channel List
-channel_list = []
-
+channel_list = ['general']
+usernames_and_chats = [{}]
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     return render_template('index.html', channel_list=channel_list)
 
+
 @socketio.on('emit_newchannel')
 def proc(data):
     newchannel = data['newchannel']
-    channel_list.append(newchannel)
-    emit('response', {'resp_newchannel': channel_list}, broadcast=True)
+    newchannel = newchannel.lower()
+    if newchannel in channel_list:
+        error = 'exist'
+        emit('response', {'resp_newchannel': error}, broadcast=False)
+    else:
+        channel_list.append(newchannel)
+        emit('response', {'resp_newchannel': channel_list}, broadcast=True)
